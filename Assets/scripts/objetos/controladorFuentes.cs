ac271953a16a7bbe4b82b366b13f27e71bfc9c19
pabloHeit <106784765+pabloHeit@users.noreply.github.com>
2022-07-15 16:26:57 -0300

    -Fuentes de colores 1
    -Creado el script controladorFuentes(vacio por ahora)
    -Agregados y recortados los sprites de las fuentes

---
+++ Assets/scripts/objetos/controladorFuentes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorFuentes : MonoBehaviour
+{
+    [SerializeField] private int color;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
