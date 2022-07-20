91c52a5b83fb643277827f23dfa7c670df451afc
pabloHeit <pabloaheit@yahoo.com.ar>
2022-07-20 19:31:15 -0300

    Mercader 1
    -Ahora el texto del dialogo del mercader se actualiza progresivamente
    -Dialogos aleatorios

--- Assets/scripts/npc/dialogoVendedor.cs
+++ Assets/scripts/npc/dialogoVendedor.cs
@@ -8,21 +8,38 @@ public class dialogoVendedor : MonoBehaviour
     [SerializeField] private Transform textoPanelPos;
     [SerializeField] private GameObject textoPanel;
     [SerializeField] private TMP_Text textoText;
-    [SerializeField] private string dialogueLine;
-    private bool dialogoActivo=false;
+    private string dialogueLine;
+    private bool dialogoActivo = false;
+
+    [SerializeField] private string[] dialogos;
+    private int indexDialogo;
+    private float dialogueTime = 0.1f;
+
+    
+    private void Start()
+    {
+        if (dialogos.Length == 0){
+            Debug.Log("<color=red>dialogoVendedor ERROR</color>     El mercader no tiene dialogos");
+        }        
+    }
 
     private void MensajePantalla(){
+
         dialogoActivo = true;
-        textoText.text = dialogueLine;
         textoPanel.SetActive(true);
+        dialogueLine = dialogos[ Random.Range(0, dialogos.Length) ];
+
+        StartCoroutine(mostrarDialogo());
     }
+
     private void ApagarMensaje(){
+        textoText.text = "";
         textoPanel.SetActive(false);
         dialogoActivo = false;
     }  
 
     private void OnTriggerEnter2D(Collider2D other){
-        if (other.CompareTag("Player") && !dialogoActivo){         
+        if (other.CompareTag("Player") && !dialogoActivo){
             MensajePantalla();
         }
     }
@@ -30,5 +47,16 @@ public class dialogoVendedor : MonoBehaviour
         if (other.CompareTag("Player")){
             ApagarMensaje();
         }
+    }
+
+    IEnumerator mostrarDialogo()
+    {
+        foreach (var letra in dialogueLine){
+            
+            if (dialogoActivo == false) break;
+            textoText.text += letra;
+            if (letra.ToString() != " ") /*then*/ yield return new WaitForSeconds(dialogueTime);
+        }
+        
     }
 }
