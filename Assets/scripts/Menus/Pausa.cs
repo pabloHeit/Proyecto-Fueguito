54471231e196ed2cf35576c00ddf6b8852740ff0
pabloHeit <pabloaheit@yahoo.com.ar>
2022-07-31 23:28:01 -0300

    Pausa opciones
    -Funciona el botos de opciones, que te lleva a un submenu que te permite cambiar el volumen con un slider (a desarrollar).

--- Assets/scripts/Menus/Pausa.cs
+++ Assets/scripts/Menus/Pausa.cs
@@ -5,21 +5,26 @@ using UnityEngine.UI;
 
 public class Pausa : MonoBehaviour
 {
-    [SerializeField] private GameObject PauseMenu;
-    public Button continueButton, opcionesButton, exitButton;
+    [SerializeField] private GameObject PauseMenu, IndexMenu, OptionsMenu;
+    public Button continueButton, opcionesButton, exitButton, volverButton;
     private bool enPausa = false;
+    private bool enOpciones = false;
 
     void Start()
     {
         Button continueBtn = continueButton.GetComponent<Button>();
         Button opcionesBtn = opcionesButton.GetComponent<Button>();
         Button exitBtn = exitButton.GetComponent<Button>();
+        Button volverBtn = volverButton.GetComponent<Button>();
 
         continueBtn.onClick.AddListener(AlternarPausa);
-        //opcionesBtn.onClick.AddListener(AlternarOpciones);
+        opcionesBtn.onClick.AddListener(AlternarOpciones);
         exitBtn.onClick.AddListener(Application.Quit);
-        
+        volverBtn.onClick.AddListener(AlternarOpciones);
+
         PauseMenu.SetActive(enPausa);
+        IndexMenu.SetActive(enPausa);
+        OptionsMenu.SetActive(enOpciones);
     }
 
     void Update()
@@ -32,7 +37,20 @@ public class Pausa : MonoBehaviour
     {
         enPausa = !enPausa;
         if (enPausa) Time.timeScale = 0;
-        else Time.timeScale = 1;
+        else{
+            Time.timeScale = 1;
+            enOpciones = false;
+            OptionsMenu.SetActive(enOpciones);
+        }
         PauseMenu.SetActive(enPausa);
+        IndexMenu.SetActive(enPausa);
     }
+
+    public void AlternarOpciones()
+    {
+        enOpciones = !enOpciones;
+        IndexMenu.SetActive(!enOpciones);
+        
+        OptionsMenu.SetActive(enOpciones);
+    }    
 }
