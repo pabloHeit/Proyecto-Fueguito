88b703f58f5cc1a29ab18d49f986194bf3787f45
pabloHeit <pabloaheit@yahoo.com.ar>
2022-11-06 22:03:45 -0300

    Puertas arregladas
    +Ahora las puertas se cierran al entrar en ellas.

--- Assets/scripts/Mapa/encima.cs
+++ Assets/scripts/Mapa/encima.cs
@@ -5,46 +5,37 @@ using UnityEngine;
 public class encima : MonoBehaviour
 {
     private Animator animator;
+    private int i = 0;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        Debug.Log("gol");
-         foreach (Transform child in transform) {
-            foreach (Transform child2 in transform) {
-                Debug.Log(child2.name);
-                if (child2.name == "puerta(Clone)" ||  child2.name == "puerta izq(Clone)" || child2.name == "puerta arriba(Clone)" || child2.name == "puerta abajo(Clone)")
-                {
-                    animator = child2.GetComponent<Animator>();
-                    Debug.Log("gol2");
-                    animator.SetBool("puertaAbierta",true);
-                }
+        StartCoroutine(ConfigurarPuertasInicio(1f));
+    }
+
+    public IEnumerator ConfigurarPuertasInicio(float tiempoMargen)
+    {
+        yield return new WaitForSeconds(tiempoMargen);
+
+        foreach (Transform child in transform) {
+            var child2 = child.GetChild(0);
+            if (child2.name == "puerta(Clone)" ||  child2.name == "puerta izq(Clone)" || child2.name == "puerta arriba(Clone)" || child2.name == "puerta abajo(Clone)")
+            {
+                animator = child2.GetComponent<Animator>();
+                animator.SetBool("puertaAbierta",true);
             }
+        }        
     }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    /* private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if ((other.CompareTag("Player"))) {
-            Debug.Log("entro");
             //falta preguntar si hay bichos
            foreach (Transform child in transform) {
-            foreach (Transform child2 in transform) {
-                Debug.Log(child2.name);
+                var child2 = child.GetChild(0);
                 if (child2.name == "puerta(Clone)" ||  child2.name == "puerta izq(Clone)" || child2.name == "puerta arriba(Clone)" || child2.name == "puerta abajo(Clone)")
                 {
                     animator = child2.GetComponent<Animator>();
                     animator.SetTrigger("encierro");
                 }
             }
-    }
-
-            
         }
-    } */
+    }
 }
